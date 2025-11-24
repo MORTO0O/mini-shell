07a0518d91f9577ff4f49c3009ec18758d15ea71
Егор Корнеев <eakorneev@mai.education>
2025-11-24 14:56:22 +0300

    Revert "tar+zip"
    This reverts commit 9199925ca8dc8a1ebc3e63572dfe9048a84ec0d5.

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,4 @@
-import pathlib
-LOG_FILE = str(pathlib.Path(__file__).resolve().parent.parent / "shell.log")
+LOG_FILE = "shell.log"
 HIST_FILE = ".history"
 COUNTER_FILE = ".history_counter"
 TRASH_DIR = ".trash"
